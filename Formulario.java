0c8da6ef87b9a838ed140493d00a539fdcc8e781
Asahel <thespyke@outlook.es>
2024-02-01 11:11:51 -0600

    añadiendo JTextField

--- Formulario.java
+++ Formulario.java
@@ -5,28 +5,19 @@ import java.awt.event.*;
 
 public class Formulario extends JFrame implements ActionListener {
 
-    private JButton boton1, boton2, boton3;
-
+    private JTextField textField1;
+    private JButton boton1;
     private  JLabel etiqueta1;
     
     public Formulario(){
+
+        
 
         setLayout(null);
         boton1 = new JButton("1");
         boton1.setBounds(10,100,90,30);
         add(boton1);
         boton1.addActionListener(this);
-
-        boton2 = new JButton("2");
-        boton2.setBounds(110,100,90,30);
-        add(boton2);
-        boton2.addActionListener(this);
-
-        boton3 =new JButton("3");
-        boton3.setBounds(210, 100, 90, 30);
-        add(boton3);
-        boton3.addActionListener(this);
-        
 
     }
 
