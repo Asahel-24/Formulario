817f1a7868e633da1913ec8a7ebaee2549ebb88c
Asahel <thespyke@outlook.es>
2024-02-01 10:40:44 -0600

    Update Formulario.java

--- Formulario.java
+++ Formulario.java
@@ -1,26 +1,34 @@
+package formulario;
+
 import javax.swing.*;
 import java.awt.event.*;
 
 public class Formulario extends JFrame implements ActionListener {
 
-    JButton boton1;
-   private  JLabel etiqueta1;
-   private JLabel etiqueta2;
+    private JButton boton1, boton2, boton3;
+
+    private  JLabel etiqueta1;
     
     public Formulario(){
 
         setLayout(null);
-        boton1 = new JButton("Cerrar");
-        boton1.setBounds(300,250,100,30);
+        boton1 = new JButton("1");
+        boton1.setBounds(10,100,90,30);
         add(boton1);
         boton1.addActionListener(this);
 
-        /*etiqueta1 = new JLabel("Interfaz Grafica");
-        etiqueta1.setBounds(10,20,300,30);
-        add(etiqueta1);
-        etiqueta2 = new JLabel("Version 1.0");
-        etiqueta2.setBounds(10,100,100,30);
-        add(etiqueta2);*/
+        boton2 = new JButton("2");
+        boton2.setBounds(110,100,90,30);
+        add(boton2);
+        boton2.addActionListener(this);
+
+        boton3 =new JButton("3");
+        boton3.setBounds(210, 100, 90, 30);
+        add(boton3);
+        boton3.addActionListener(this);
+
+
+
     }
 
     public void actionPerformed(ActionEvent e){
