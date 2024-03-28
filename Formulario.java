b07a6bea205f2ef3ba1bf871fbf961b2fc45bdd4
Asahel <thespyke@outlook.es>
2024-03-28 14:24:15 -0600

    Jugando con los colores RGB

--- Formulario.java
+++ Formulario.java
@@ -1,63 +1,73 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
-public class Formulario extends JFrame implements ActionListener {
-
-    private JTextField textField1, textField2;
+public class Formulario extends JFrame implements ActionListener{
+    private JLabel label1, label2, label3;
+    private JComboBox combo1, combo2, combo3;
     private JButton boton1;
-    private  JLabel etiqueta1,etiqueta2, etiqueta3;
-    
+
     public Formulario(){
-
-        setTitle("Formulario");
-
         setLayout(null);
-        etiqueta1 = new JLabel("Valor1:");
-        etiqueta1.setBounds(50, 5,100,30);
-        add(etiqueta1);
+        label1 = new JLabel("Rojo:");
+        label1.setBounds(10,10,100,10);
+        add(label1);
 
-        etiqueta2 = new JLabel("Valor 2:");
-        etiqueta2.setBounds(50, 35, 100,30);
-        add(etiqueta2);
+        combo1 = new JComboBox();
+        combo1.setBounds(120,10,50,30);
+        for (int i = 0; i <= 255; i++){
+            combo1.addItem(String.valueOf(i));
+        }
+        add(combo1);
 
-        etiqueta3= new JLabel("Resultado:");
-        etiqueta3.setBounds(120, 80, 100,30);
-        add(etiqueta3);
+        label2 = new JLabel("Verde:");
+        label2.setBounds(10,50,100,30);
+        add(label2);
 
-        textField1 = new JTextField();
-        textField1.setBounds(120, 10,150,20);
-        add(textField1);
+        combo2 = new JComboBox();
+        combo2.setBounds(120,50,50,30);
+        for (int i = 0; i <=255; i++){
+            combo2.addItem(String.valueOf(i));
+        }
+        add(combo2);
 
-        textField2 = new JTextField();
-        textField2.setBounds(120,40,150,20);
-        add(textField2);
+        label3 = new JLabel("Azul:");
+        label3.setBounds(10,90,100,10);
+        add(label3);
 
+        combo3 = new JComboBox();
+        combo3.setBounds(120,90,50,30);
+        for (int i = 0; i <= 255; i++){
+            combo3.addItem(String.valueOf(i));
+        }
+        add(combo3);
 
-        boton1 = new JButton("Sumar");
-        boton1.setBounds(10,80,100,30);
+        boton1 = new JButton("Fijar color");
+        boton1.setBounds(10,130,100,30);
         add(boton1);
         boton1.addActionListener(this);
-
     }
 
     public void actionPerformed(ActionEvent e){
         if (e.getSource() == boton1){
-            int valor1 = 0, valor2 = 0, resultado = 0;
+            String cad1 = combo1.getSelectedItem().toString();
+            String cad2 = combo2.getSelectedItem().toString();
+            String cad3 = combo3.getSelectedItem().toString();
 
-            valor1 = Integer.parseInt(textField1.getText());
-            valor2 = Integer.parseInt(textField2.getText());
+            int rojo = Integer.parseInt(cad1);
+            int verde = Integer.parseInt(cad2);
+            int azul = Integer.parseInt(cad3);
 
-            resultado = valor1 + valor2;
-
-            etiqueta3.setText("Resultado: " + resultado);
+            Color color1 = new Color(rojo, verde, azul);
+            boton1.setBackground(color1);
         }
     }
 
-    public static void main(String args []){
+    public static void main(String args[]){
         Formulario formulario = new Formulario();
-        formulario.setBounds(0,0,300,150);
+        formulario.setBounds(0,0,190,220);
         formulario.setVisible(true);
-        formulario.setLocationRelativeTo(null);
         formulario.setResizable(false);
+        formulario.setLocationRelativeTo(null);
     }
 }
