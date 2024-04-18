9706f979e58e87072a7b62e9982af2b8b5d43ed2
Asahel <thespyke@outlook.es>
2024-04-18 14:20:20 -0600

    Modificando archivo y metiendo el metodo JRadioButton

--- Formulario.java
+++ Formulario.java
@@ -1,53 +1,49 @@
 import javax.swing.*;
 import javax.swing.event.*;
-import java.awt.*;
-import java.awt.event.*;
-public class Formulario extends JFrame implements ActionListener, ChangeListener{
-    private JLabel labael1;
-    private JCheckBox check1;
-    private JButton boton1;
+
+public class Formulario extends JFrame implements ChangeListener{
+    private JRadioButton radio1, radio2, radio3;
+    private ButtonGroup bg;
 
     public Formulario(){
         setLayout(null);
-        labael1 = new JLabel("Aceptar terminos y condiciones");
-        labael1.setBounds(10,10,400,30);
-        add(labael1);
+        bg = new ButtonGroup();
 
-        check1 = new JCheckBox("Acepto");
-        check1.setBounds(10,50,100,30);
-        check1.addChangeListener(this);
-        add(check1);
+        radio1 = new JRadioButton("640 x 480");
+        radio1.setBounds(10,20,100,30);
+        radio1.addChangeListener(this);
+        add(radio1);
+        bg.add(radio1);
 
-        boton1 = new JButton("Continuar");
-        boton1.setBounds(10,100,100,30);
-        boton1.setBackground(new Color(255, 0, 23));
-        add(boton1);
-        boton1.addActionListener(this);
-        boton1.setEnabled(false);
+        radio2 = new JRadioButton("800 x 600");
+        radio2.setBounds(10,70,100,30);
+        radio2.addChangeListener(this);
+        add(radio2);
+        bg.add(radio2);
+
+        radio3 = new JRadioButton("1024 x 768");
+        radio3.setBounds(10,120,100,30);
+        radio3.addChangeListener(this);
+        add(radio3);
+        bg.add(radio3);
     }
-//Para el chekBox
+
     public void stateChanged(ChangeEvent e){
-        if (check1.isSelected() == true){
-            boton1.setBackground(new Color(12, 211, 22));
-            boton1.setEnabled(true);
-        } else {
-            boton1.setBackground(new Color(255, 0, 23));
-            boton1.setEnabled(false);
+        if (radio1.isSelected()){
+            setSize(640,480);
         }
-    }
-//Para el boton aceptar
-    public void actionPerformed(ActionEvent e){
-        if (e.getSource() == boton1){
-            System.exit(0);
+        if (radio2.isSelected()){
+            setSize(800,600);
+        }
+        if (radio3.isSelected()){
+            setSize(1024,768);
         }
     }
 
     public static void main(String args[]){
         Formulario formulario = new Formulario();
-        formulario.setBounds(0,0,350,200);
-        formulario.setResizable(false);
+        formulario.setBounds(0,0,350,230);
         formulario.setVisible(true);
-        formulario.setLocationRelativeTo(null);
+        formulario.setResizable(false);
     }
-
 }
